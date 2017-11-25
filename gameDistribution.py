e9dcc90464e1ba3a1cc273ebea2a65ad4ae46d61
kloseliebej <kloseliebej@gmail.com>
2017-11-25 11:58:21 -0600

    add signup

--- gameDistribution.py
+++ gameDistribution.py
@@ -8,7 +8,13 @@ app.config.from_object('config')
 @app.route('/')
 def index():
     if 'user' in session:
-        return render_template('manager_profile.html', session=session)
+        print session
+        if session['user_type'] == 'manager':
+            return render_template('manager_profile.html', session=session)
+        elif session['user_type'] == 'gamer':
+            return render_template('manager_profile.html', session=session)
+        else:
+            return render_template('manager_profile.html', session=session)
     else:
         return redirect(url_for('login'), 302)
 
@@ -18,8 +24,15 @@ def login():
         name = request.form['user']
         passwd = request.form['passwd']
         cursor = g.db.execute('select * from users where name=? and password=?', [name, passwd])
-        if cursor.fetchone() is not None:
+        data = cursor.fetchone()
+        if data is not None:
             session['user'] = name
+            if data[4] == 1:
+                session['user_type'] = 'gamer'
+            elif data[5] == 1:
+                session['user_type'] = 'developer'
+            else:
+                session['user_type'] = 'manager'
             print 'Login successfully!'
             return redirect(url_for('index'))
         else:
@@ -32,6 +45,32 @@ def login():
 def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        passwd = request.form['passwd']
+        if request.form['type']=='gamer':
+            isGamer = 1
+            isDeveloper = 0
+        else:
+            isDeveloper = 1
+            isGamer = 0
+        cursor = g.db.execute('select * from users where email=?', [email])
+        if cursor.fetchone() is None:
+            session['user'] = name
+            session['user_type'] = request.form['type']
+            print "signup succesfully!"
+            g.db.execute('insert into users (name, password, email, isGamer, isDeveloper, isManager) values (?,?,?,?,?,0)',
+                         [name, passwd, email, isGamer , isDeveloper])
+            return redirect(url_for('index'))
+        else:
+            print "user already exist!"
+            return redirect(url_for('signup'))
+    else:
+        return render_template('signup.html')
 
 @app.before_request
 def before_request():
